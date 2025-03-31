bb4a9b662286663722b3ba1df631d8761cbb2ea9
oriole1 <nvme12@proton.me>
2025-03-31 19:39:43 +0700

    Update cliapps.sh

--- cliapps.sh
+++ cliapps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
- curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && sudo apt install flatpak && sudo apt install gnome-software-plugin-flatpak && flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+ curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && sudo apt install flatpak build-essential && brew install gcc && sudo apt install gnome-software-plugin-flatpak && flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
