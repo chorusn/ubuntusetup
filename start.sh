00de5decfd612240401c8909481fc813cdd59d93
oriole1 <nvme12@proton.me>
2025-04-07 20:16:20 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,3 @@
+/bin/bash
+
+sudo apt install git nala && sudo apt update && sudo apt upgrade && sudo snap install thunderbird firefox snap-store && git clone https://github.com/oriole1/ubuntusetup && cd ubuntusetup && chmod +x de.sh && chmod +x secure.sh && chmod +x cliapps.sh && ./de.sh && ./cliapps.sh
