de80ec2b0453a89323a473c351957fe6d83d089c
oriole1 <nvme12@proton.me>
2025-04-08 16:24:40 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install git nala && sudo apt update && sudo apt upgrade && sudo snap install thunderbird firefox snap-store && chmod +x de.sh && chmod +x secure.sh && chmod +x ollama.sh && chmod +x updater.sh && chmod +x apps.sh && chmod +x cliapps.sh && ./de.sh && ./cliapps.sh
+sudo apt install git nala curl && sudo apt update && sudo apt upgrade && sudo snap install thunderbird firefox snap-store && chmod +x de.sh && chmod +x secure.sh && chmod +x ollama.sh && chmod +x updater.sh && chmod +x apps.sh && chmod +x cliapps.sh && ./de.sh && ./cliapps.sh && ./secure.sh && ./apps.sh
